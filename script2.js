f9525c2157212f5d3582b5cc051ae03fae8616f3
Kam Rodriguez <kamrod1021@gmail.com>
2025-10-26 19:18:21 -0700

    I got it this far, I believe I'm just missing something small. I got the numbers to print out and the fizz and buzz to do so as well.

--- script2.js
+++ script2.js
@@ -9,3 +9,17 @@
 // 14
 // FizzBuzz
 // ...
+
+for (let i = 1; i <= 100; i++) {
+  
+  if (i % 3 === 0) {
+    console.log("Fizz");
+}
+ if (i % 5 === 0) {
+    console.log ("Buzz");
+}
+  else if (i %3 === 0 && i % 5 === 0) {
+    console.log ("FizzBuzz");
+}
+  console.log(i)
+}
